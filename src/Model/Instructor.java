1e61276e9395298fbc9f45677b49b9a088f91cf5
rusaro scovia <isaroscovi@gmail.com>
2026-02-24 15:36:40 +0200

    Added Instructor class for Lab 1

--- src/Model/Instructor.java
+++ src/Model/Instructor.java
@@ -1,4 +1,20 @@
 package Model;
 
-public class Instructor {
+public class Instructor extends Person {
+
+    private String department;
+
+    public Instructor(String name, String email, String department) {
+        super(name, email);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String getRole() {
+        return "Instructor";
+    }
 }
