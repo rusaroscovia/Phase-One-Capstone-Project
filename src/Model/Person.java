2e8d3d5e486fb76102fd450910c9bfbab2cc3e8f
rusaro scovia <isaroscovi@gmail.com>
2026-02-24 15:47:25 +0200

    Refactored Person class

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -5,6 +5,7 @@ public abstract class Person {
     private String name;
     private String email;
 
+
     public Person(String name, String email) {
         this.name = name;
         this.email = email;
